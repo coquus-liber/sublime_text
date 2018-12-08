2f578c908b2c71c66b89fa8ba123dd515b573f6b
Robert W Ferney <robert.w.ferney@uscis.dhs.gov>
2018-12-08 06:52:42 +0000

    install ok

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,14 @@
 # Cookbook:: sublime_text
 # Recipe:: default
 #
-# Copyright:: 2018, The Authors, All Rights Reserved.
+
+# https://www.sublimetext.com/docs/3/linux_repositories.html#apt
+
+apt_repository 'sublimetext' do
+  distribution "apt/stable/"
+  key          "https://download.sublimetext.com/sublimehq-pub.gpg"
+  uri          "https://download.sublimetext.com/"
+end
+
+apt_package 'sublime-text'
+
